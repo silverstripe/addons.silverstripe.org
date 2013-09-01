72f060f72d0608c4e0a7f4a8f3bf3941daa40d6b
Ingo Schommer <ingo@silverstripe.com>
2013-09-02 00:31:50 +0200

    Fixed staging and prod paths and users for Capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server "addons.silverstripe.org", :app, :web, :db, :primary => true
-set :deploy_to, "/sites/extensions"
-set :user, "extensions"
-set :webserver_group, "extensions"
+set :deploy_to, "/sites/addons"
+set :user, "addons"
+set :webserver_group, "addons"
 set :port, 2222
