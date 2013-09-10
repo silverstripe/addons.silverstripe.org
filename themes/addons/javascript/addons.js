7b377c272934011953368f2d75745f4ed31cea9c
Ingo Schommer <ingo@silverstripe.com>
2013-09-10 16:46:05 +0200

    Fixed JS selector

--- themes/addons/javascript/addons.js
+++ themes/addons/javascript/addons.js
@@ -7,7 +7,7 @@ jQuery(function($) {
 	$(document).ready(function() {
 		// Show 2.4 specific notice if box is ticked
 		$('#legacy-search-notice').hide();
-		$('input[name="compatibility[]"').click(function(e) {
+		$('input[name="compatibility[]"]').click(function(e) {
 			var doShow = $('input[name="compatibility[]"][value="2.4"]').is(':checked');
 			$('#legacy-search-notice')[doShow ? 'show' : 'hide']();
 		});
