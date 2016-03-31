7cba9cde5990562ac0c857820154fdd4f378f8de
Cam Findlay <cam@silverstripe.com>
2016-03-31 16:47:00 +1300

    MINOR remove reference to old URL

--- themes/addons/javascript/addons.js
+++ themes/addons/javascript/addons.js
@@ -3,16 +3,7 @@ jQuery(function($) {
 		$(this).parents("#readme").toggleClass("collapsed");
 		return false;
 	});
-
-	$(document).ready(function() {
-		// Show 2.4 specific notice if box is ticked
-		$('#legacy-search-notice').hide();
-		$('input[name="compatibility[]"]').click(function(e) {
-			var doShow = $('input[name="compatibility[]"][value="2.4"]').is(':checked');
-			$('#legacy-search-notice')[doShow ? 'show' : 'hide']();
-		});
-	});
-
+	
 	// Bootstrap tooltips
 	$('[data-toggle=tooltip]').tooltip();
 });
