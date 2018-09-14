5c4d75d3bdc7ebca8bbedea09060f03f7f42493f
Robbie Averill <robbie@silverstripe.com>
2018-09-14 16:20:53 +0200

    Add a monolog syslogger to enable module rating check suites to report failures to Graylog

--- mysite/code/services/AddonBuilder.php
+++ mysite/code/services/AddonBuilder.php
@@ -2,6 +2,7 @@
 
 use Composer\Package\Package;
 use Composer\Package\PackageInterface;
+use Monolog\Handler\SyslogHandler;
 use SilverStripe\ModuleRatings\CheckSuite;
 
 /**
@@ -29,7 +30,14 @@ class AddonBuilder
     public function build(Addon $addon)
     {
         putenv("GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no\"");
-        $this->setCheckSuite(new CheckSuite());
+
+        $checkSuite = new CheckSuite();
+        $this->setCheckSuite($checkSuite);
+        // Provide the checksuite with a logger in case API calls fail
+        $logger = new Monolog\Logger('module_ratings_logs', [
+            new SyslogHandler('SilverStripe_log'),
+        ]);
+        $checkSuite->setLogger($logger);
 
         $composer = $this->packagist->getComposer();
         $downloader = $composer->getDownloadManager();
