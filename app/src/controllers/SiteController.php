78bd11e8bb18ff53175ae5f196ae73e011b338fd
Mikaela Young <mikaela.young18@gmail.com>
2019-08-12 17:51:27 +1200

    Add same jquery dependency as toolbar to fix order errors

--- app/src/controllers/SiteController.php
+++ app/src/controllers/SiteController.php
@@ -17,6 +17,7 @@ class SiteController extends Controller
     {
         RSSFeed::linkToFeed("add-ons/rss", "New modules on addons.silverstripe.org");
 
+        Requirements::javascript("//cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js");
         Requirements::javascript("themes/addons/bootstrap/js/bootstrap.min.js");
         Requirements::javascript("themes/addons/javascript/addons.js");
         Requirements::javascript("//www.google.com/jsapi");
