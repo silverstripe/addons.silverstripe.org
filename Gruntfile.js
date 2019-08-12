d39b115084ad0fe9f7b94a02006c2a83ec9ead54
Mikaela Young <mikaela.young18@gmail.com>
2019-08-13 11:14:43 +1200

    Remove conflicting bootstrap modal JS, add uglify to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,40 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
-		watch: {
+    watch: {
       files: ['themes/addons/**/*.less'],
       tasks: 'less:debug'
     },
-		less: {
-			debug: {
-		    options: {
-					paths: ['themes/addons/less/'],
-					yuicompress: false
-		    },
-		    files: {
-					"themes/addons/css/addons.css": "themes/addons/less/addons.less"
-		    }
-		  },
-		  production: {
-		    options: {
-					paths: ['themes/addons/less/'],
-					yuicompress: true
-		    },
-		    files: {
-					"themes/addons/css/addons.css": "themes/addons/less/addons.less"
-		    }
-		  }
-		}
+    less: {
+      debug: {
+        options: {
+          paths: ['themes/addons/less/'],
+          yuicompress: false
+        },
+        files: {
+          "themes/addons/css/addons.css": "themes/addons/less/addons.less"
+        }
+      },
+      production: {
+        options: {
+          paths: ['themes/addons/less/'],
+          yuicompress: true
+        },
+        files: {
+          "themes/addons/css/addons.css": "themes/addons/less/addons.less"
+        }
+      }
+    },
+    uglify: {
+      bootstrap: {
+        files: {
+          "themes/addons/bootstrap/js/bootstrap.min.js": "themes/addons/bootstrap/js/bootstrap.js"
+        }
+      }
+    }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 };
